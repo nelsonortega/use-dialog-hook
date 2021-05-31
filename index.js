173d0b102a9897fa2d42873934b3c11eaa7595ae
Nelson <nelson.ortega@microfocus.com>
2021-05-31 13:23:46 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+import App from './src/App'
+import { AppRegistry } from 'react-native'
+import { name as useDialogHook } from './app.json'
+
+AppRegistry.registerComponent(useDialogHook, () => App)
